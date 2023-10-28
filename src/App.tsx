4b4ddc208a2a8e7423c7e9ad77ba13bedcc1d99e
Brzozowski-Piotr-EDU <piobrzedu@icloud.com>
2023-10-28 17:53:01 +0200

    Pierwszy commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+export const FullName = () => {
+  const fullName: string = "Piotr Brzozowski";
+
+  return <div className="container">Full name: {fullName}</div>;
+};
+
+export const Birthday = () => {
+  const birthDate: Date = new Date("1999-08-12");
+  const dateToDisplay: string = birthDate.toLocaleDateString("pl-PL", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  return <div className="container">Birthday: {dateToDisplay}</div>;
+};
+
+export const PresentSuggestion = () => {
+  const presentSuggestions: string[] = [
+    "Book 101 useful tips",
+    "Pair of good heaphones",
+    "Ticket to the cinema",
+    "Gift card to local clothing store",
+    "Dinner on good restraunt",
+    "Just your time",
+  ];
+
+  const suggestedPresentIndex: number = Math.round(Math.random() * 5);
+
+  const presentProposition: string = presentSuggestions[suggestedPresentIndex];
+
+  return (
+    <div className="container">You can buy him/her: {presentProposition}</div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <FullName />
+      <Birthday />
+      <PresentSuggestion />
     </div>
   );
 }
